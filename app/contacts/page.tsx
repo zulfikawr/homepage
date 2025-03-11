6deb179fea8d4e2590129ccff8d1d41e5b66852a
zulfikar6556 <zulfikar6556@gmail.com>
2025-03-11 15:18:38 +0000

    add contact page

---
+++ app/contacts/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from 'next';
+import ContactsContent from './content';
+
+export const metadata: Metadata = {
+  title: 'Contacts - Zulfikar',
+};
+
+export default function ContactsPage() {
+  return <ContactsContent />;
+}
