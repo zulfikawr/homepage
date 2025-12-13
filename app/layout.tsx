2edff0a8638f5b31d07d59d0dd4feda94c98c1ac
zulfikar <155657912+zulfikawr@users.noreply.github.com>
2025-12-13 15:19:46 +0000

    add cloudflare web analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import Providers from './providers';
 import Drawer from '@/components/Drawer';
 import Modal from '@/components/Modal';
@@ -74,6 +75,12 @@ export default function RootLayout({
           <Modal />
           <Toast />
         </Providers>
+        {/* Cloudflare Web Analytics */}
+        <Script
+          defer
+          src='https://static.cloudflareinsights.com/beacon.min.js'
+          data-cf-beacon='{"token": "1eba07f9cc5547d696b384c99c479a21"}'
+        />
       </body>
     </html>
   );
